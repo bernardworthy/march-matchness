dca957068b7cef9e6e9c6c7069eb512ec1433b14
Bernard <worthybernard@gmail.com>
2015-02-20 20:35:07 -0500

    Changing flipping of tiles to jquery and not checkbox

--- src/js/game.js
+++ src/js/game.js
@@ -27,13 +27,56 @@ app.router.add('easy', function(){
     };
 
     var numPairs = 9;
-
     var cardsArr = app.getCards(numPairs);
-
-    console.log(cardsArr);
-
     var template = _.template($('#tile').html(), { variable: 'm'});
-    $('.grid').html(template({ deck: cardsArr}));
 
+    $('.grid').html(template({ deck: cardsArr}));
   }
+
+  // $('.grid-cell').click(function(e) {
+  //   var tile = e.target;
+  //   console.log(tile);
+  //   // e.preventDefault();
+  //   // e && (e.preventDefault() || e.stopPropagation());
+  // });
+  $('.grid').on('click', '.grid-cell', function(evt) {
+    var tile = $(this);
+
+    console.log(tile);
+
+    if ($('.clicked').length > 0) {
+      console.log('we need to compare');
+      var firstTxt = $('.clicked')[0].innerText;
+      var secondTxt = $('.grid-icon', tile)[0].innerText;
+
+      if (firstTxt === secondTxt) {
+        console.log('got a match');
+      } else {
+        console.log('not a match');
+        var clickedTile = $('.clicked');
+        clickedTile.toggleClass('grid-cell-not-clicked');
+        $('.grid-icon', clickedTile).toggleClass('grid-icon-not-clicked');
+        tile.toggleClass('grid-cell-not-clicked');
+        $('.grid-icon', tile).toggleClass('grid-icon-not-clicked');
+        $(tile).removeClass('clicked');
+      }
+      // } else {
+      //   console.log('take away a life');
+      //   var clickedTile = $('.clicked')[0];
+      //
+      //   $(clickedTile).toggleClass('grid-cell-not-clicked');
+      //   $('.grid-icon', clickedTile).toggleClass('grid-icon-not-clicked');
+      //
+      //   $(tile).toggleClass('grid-cell-not-clicked');
+      //   $('.grid-icon', tile).toggleClass('grid-icon-not-clicked');
+      // }
+    } else {
+      $(tile).toggleClass('grid-cell-not-clicked');
+      $('.grid-icon', tile).toggleClass('grid-icon-not-clicked');
+      $(tile).addClass('clicked');
+      console.log('just added the class');
+    }
+    // evt.stopPropagation();
+    // evt.preventDefault();
+  });
 });
